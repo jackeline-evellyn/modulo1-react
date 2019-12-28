5ef38f3d4d6e36f997af8a54286a1f9f1b0a4d0d
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2019-12-28 14:25:11 -0300

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-alert ('Hello Rocketseat');
+import React, {Component, Fragment} from 'react';
+import {render} from 'react-dom';
+
+class Button extends Component {
+    render(){
+        return <a href="">Salvar</a>
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+   // Após o return precisa de um tag html por volta
+        <Fragment>
+            {/* Fragment funciona da mesma forma sem ultilizar pré formatação */}
+            <h1>Hello Rocketseat</h1>
+            {/* Chama o component Button dentro do Component App */}
+            <Button />
+        </Fragment>
+        );
+    }
+}
+
+render(<App />, document.getElementById("app"));
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(_dirname, 'src', index.js),
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    // __dirname retornará o  diretório atual do arquivo que esta sendo editado
+    //a cada virgula passada é como se fosse uma barra
+
+    //output é onde ficará o arquivo transpilado
+    output:{
+        path: path.resolve(__dirname, 'public'),
+        //nome do arquivo que quer gerar
+        filename: 'bundle.js'
+    },
+    devServer: {
+        //a pasta que o dev-service precisa ficar monitorando
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: "babel-loader"
+              }
+          }  
+        ]
+    },
 };
